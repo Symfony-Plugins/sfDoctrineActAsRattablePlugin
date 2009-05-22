433359e5cce271adb78298dc44911e86282b39c6
Mickael.Kurmann <Mickael.Kurmann@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-05-22 15:26:20 -0500

    ajout des tests unitaires des base ajout de l'option de rounding ajout de l'option d'utilisateur mise à jour du readme mise à jour du schema
    git-svn-id: http://svn.symfony-project.com/plugins/sfDoctrineActAsRattablePlugin/trunk@18560 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/Rattable.class.php
+++ lib/Rattable.class.php
@@ -6,18 +6,23 @@
  */
 class Doctrine_Rattable extends Doctrine_Record_Generator
 {
-    protected $_options = array(
+  protected $_options = array(
                             'className'     => '%CLASS%Rate',
-                            'fields'        => array(),
+                            'tableName'     => false,
                             'generateFiles' => false,
                             'table'         => false,
                             'pluginTable'   => false,
                             'children'      => array(),
                             'options'       => array(),
-                            'criterias'     => 'rate',
+                            'criterias'     => array('rate'),
                             'max_rate'      => 5,
+                            'rounding_rate' => 1,
                             'with_comment'  => true,
-    );
+                            'user'          => array('class' => 'sfGuardUser',
+                                                     'type'  => 'integer',
+                                                     'size'  => 4),
+  );
+
 
     /**
      * __construct
@@ -25,16 +30,16 @@ class Doctrine_Rattable extends Doctrine_Record_Generator
      * @param string $options
      * @return void
      */
-    public function __construct($options)
-    {
-        $this->_options = Doctrine_Lib::arrayDeepMerge($this->_options, $options);
-    }
+  public function __construct($options)
+  {
+    $this->_options = Doctrine_Lib::arrayDeepMerge($this->_options, $options);
+  }
 
-    public function buildRelation()
-    {
-        $this->buildForeignRelation('Rate');
-        $this->buildLocalRelation();
-    }
+  public function buildRelation()
+  {
+    $this->buildForeignRelation('Rate');
+    $this->buildLocalRelation();
+  }
 
     /**
      * buildDefinition
@@ -42,61 +47,65 @@ class Doctrine_Rattable extends Doctrine_Record_Generator
      * @param object $Doctrine_Table
      * @return void
      */
-    public function setTableDefinition()
+  public function setTableDefinition()
+  {
+    if(is_int($this->_options['rounding_rate']))
     {
-        $options = array('className' => $this->_options['className']);
-
-        if(is_int($this->_options['rounding_rate']))
-        {
-            $type = 'integer';
-            $length = 4;
-        }
-        else
-        {
-            $type = 'float';
-            $length = null;
-        }
-
-        $options['range'] = array(0 => $this->_options['max_rate']);
-
-        foreach ($this->_options['criterias'] as $fieldName)
-        {
-            $this->hasColumn($fieldName, $type, $length, $options);
-        }
-
-
-        $options = $this->_options['options'];
-
-        if($this->_options['with_comment'])
-        {
-            $this->hasColumn('comment', 'string', 4000, $options);
-        }
-
-
-//        // Rewrite any relations to our original table
-//        $originalName = $this->_options['table']->getClassnameToReturn();
-//        $relations = $this->_options['table']->getRelationParser()->getPendingRelations();
-//        foreach($relations as $table => $relation) {
-//            if ($table != $this->_table->getTableName() ) {
-//                // check that the localColumn is part of the moved col
-//                if (isset($relation['local']) && in_array($relation['local'], $this->_options['fields'])) {
-//                    // found one, let's rewrite it
-//                    $this->_options['table']->getRelationParser()->unsetPendingRelations($table);
-//
-//                    // and bind the rewritten one
-//                    $this->_table->getRelationParser()->bind($table, $relation);
-//
-//                    // now try to get the reverse relation, to rewrite it
-//                    $rp = Doctrine::getTable($table)->getRelationParser();
-//                    $others = $rp->getPendingRelation($originalName);
-//                    if (isset($others)) {
-//                        $others['class'] = $this->_table->getClassnameToReturn();
-//                        $others['alias'] = $this->_table->getClassnameToReturn();
-//                        $rp->unsetPendingRelations($originalName);
-//                        $rp->bind($this->_table->getClassnameToReturn() ,$others);
-//                    }
-//                }
-//            }
-//        }
+      $type = 'integer';
+      $length = 4;
     }
+    else
+    {
+      $type = 'float';
+      $length = null;
+    }
+
+    $options['range'] = array(1 => $this->_options['max_rate']);
+
+    foreach ($this->_options['criterias'] as $fieldName)
+    {
+      $this->hasColumn($fieldName, $type, $length, $options);
+    }
+    unset($options['range']);
+
+    if($this->_options['with_comment'])
+    {
+      $this->hasColumn('comment', 'string', 4000, $options);
+    }
+
+    if($this->_options['user'])
+    {
+      $user = $this->_options['user'];
+      $this->hasColumn(Doctrine_Inflector::tableize($user['class']) . '_id', $user['type'], $user['size'], array('primary' => true));
+    }
+    else
+    {
+      $this->hasColumn(Doctrine_Inflector::tableize($this->_options['table']->getComponentName()) . '_id', 'integer', null, array('primary' => true));
+      $this->hasColumn('id', 'integer', null, array('primary' => true, 'autoincrement' => true));
+    }
+  }
+
+  public function buildLocalRelation()
+  {
+    // relation to the main object
+    $options['foreign'] = $this->_options['table']->getIdentifier();
+    $options['local'] = ($this->_options['user']) ? 'id' : Doctrine_Inflector::tableize($this->_options['table']->getComponentName()) . '_id';
+    $options['type'] = Doctrine_Relation::ONE;
+    $options['onDelete'] = 'CASCADE';
+    $options['onUpdate'] = 'CASCADE';
+    $this->_table->getRelationParser()->bind($this->_options['table']->getComponentName(), $options);
+
+    // relation to the user
+    if($this->_options['user'])
+    {
+      $user = $this->_options['user'];
+
+      $table = Doctrine::getTable($user['class']);
+      $options['foreign'] = $table->getIdentifier();
+      $options['local'] = Doctrine_Inflector::tableize($user['class']) . '_id';
+      $options['type'] = Doctrine_Relation::ONE;
+
+      $this->_table->getRelationParser()->bind($table->getComponentName() . ' as User', $options);
+    }
+  }
 }
--- lib/RattableTemplate.class.php
+++ lib/RattableTemplate.class.php
@@ -1,36 +1,61 @@
 <?php
- 
+
 class Rattable extends Doctrine_Template
 {
     /**
      * __construct
      *
-     * @param string $array 
+     * @param string $array
      * @return void
      */
-    public function __construct(array $options = array())
-    {
-	    parent::__construct($options);
-        $this->_plugin = new Doctrine_Rattable($this->_options);
-    }
+  public function __construct(array $options = array())
+  {
+    parent::__construct($options);
+    $this->_plugin = new Doctrine_Rattable($this->_options);
+  }
 
     /**
      * Initialize the Rattable plugin for the template
      *
      * @return void
      */
-    public function setUp()
-    {
-        $this->_plugin->initialize($this->_table); 
-    }
+  public function setUp()
+  {
+    $this->_plugin->initialize($this->_table);
+  }
 
     /**
      * Get the plugin instance for the Rattable template
      *
      * @return void
      */
-    public function getRattable()
-    {
-        return $this->_plugin;
-    }
+  public function getRattable()
+  {
+    return $this->_plugin;
+  }
+
+  public function getRates()
+  {
+    return array();
+  }
+
+  public function getRating()
+  {
+    return array();
+  }
+
+  public function getRateCount()
+  {
+    return 0;
+  }
+
+  public function addRate()
+  {
+    return true;
+  }
+
+  public function removeRatings()
+  {
+    return false;
+  }
 }
